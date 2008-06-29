b7c142f225857762d2d6fe5fa62f11fa9ef57f21
akm2000 <akm2000@aae16913-fd4f-0410-af92-35f713787d03>
2008-06-29 03:32:58 +0000

    サンプル用にnotepad.rtfを追加しました。 examples/rubeus_swing_example01_with_class.rbをモジュールではなくクラスを使い、メソッド中でコンポーネントを組み立てるように変更しました。
    git-svn-id: https://rubeus.googlecode.com/svn/trunk@26 aae16913-fd4f-0410-af92-35f713787d03

--- examples/rubeus_swing_example01_with_class.rb
+++ examples/rubeus_swing_example01_with_class.rb
@@ -4,39 +4,42 @@ require 'rubygems'
 gem "rubeus"
 require "rubeus"
 
-module Example01
+class Example01
   extend Rubeus::Swing
   
-  # JFrameなどのコンテナのnewに渡されたブロックの中でnewされたコンポーネントは
-  # 自動的にaddされます。
-  JFrame.new("Rubeus Swing Example 01") do |frame|
-    frame.layout = BoxLayout.new(:Y_AXIS)
-    JSplitPane.new(JSplitPane::VERTICAL_SPLIT) do
-      JPanel.new do |panel|
-        panel.layout = BoxLayout.new(:X_AXIS)
-        # キーのイベントもメソッドを指定してハンドリングできます。
-        # newにブロックを渡すとlisten(:key, :pressed)が実行されます。
-        @text_field = JTextField.new do |event|
-          if event.key_code == 10 # RETURN
+  def show
+    # JFrameなどのコンテナのnewに渡されたブロックの中でnewされたコンポーネントは
+    # 自動的にaddされます。
+    JFrame.new("Rubeus Swing Example 01") do |frame|
+      frame.layout = BoxLayout.new(:Y_AXIS)
+      JSplitPane.new(JSplitPane::VERTICAL_SPLIT) do
+        JPanel.new do |panel|
+          panel.layout = BoxLayout.new(:X_AXIS)
+          # キーのイベントもメソッドを指定してハンドリングできます。
+          # newにブロックを渡すとlisten(:key, :pressed)が実行されます。
+          @text_field = JTextField.new do |event|
+            if event.key_code == 10 # RETURN
+              @textpane.text += @text_field.text + "\n"
+              @text_field.text = ''
+            end
+          end
+          # 以下のように書くこともできますです
+          # @text_field = JTextField.new
+          # @text_field.listen(:key, :key_pressed, :key_code => 10) do |event|
+          #   @textpane.text += @text_field.text + "\n"
+          #   @text_field.text = ''
+          # end
+          JButton.new("append") do
             @textpane.text += @text_field.text + "\n"
             @text_field.text = ''
           end
         end
-        # 以下のように書くこともできますです
-        # @text_field = JTextField.new
-        # @text_field.listen(:key, :key_pressed, :key_code => 10) do |event|
-        #   @textpane.text += @text_field.text + "\n"
-        #   @text_field.text = ''
-        # end
-        JButton.new("append") do
-          @textpane.text += @text_field.text + "\n"
-          @text_field.text = ''
+        JScrollPane.new(:preferred_size => [400, 250]) do |pane|
+          @textpane = JTextPane.new
         end
       end
-      JScrollPane.new(:preferred_size => [400, 250]) do |pane|
-        @textpane = JTextPane.new
-      end
+      frame.visible = true
     end
-    frame.visible = true
   end
 end
+Example01.new.show

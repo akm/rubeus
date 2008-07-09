a3d5c707291ae6480a78122caf13a30d2a0a5794
akm2000 <akm2000@aae16913-fd4f-0410-af92-35f713787d03>
2008-07-09 01:53:06 +0000

    村部さんから頂いたファイルを間違えてまだちゃんとコミットしていませんでした。すみませんー。
    git-svn-id: https://rubeus.googlecode.com/svn/trunk@95 aae16913-fd4f-0410-af92-35f713787d03

--- examples/nyanco_viewer/nyanco_viewer_rubeus.rb
+++ examples/nyanco_viewer/nyanco_viewer_rubeus.rb
@@ -1,35 +1,37 @@
 #!/usr/bin/env jruby
 #
-# にゃんこびゅーあー Rubeus 0.0.3
+# にゃんこびゅーあー Rubeus
 #
-# 2008/7/3 Junya Murabe (Lancard.com)
+# 2008/7/7 Junya Murabe (Lancard.com)
 #
 
 include Java
 
-require 'rubygems'
-gem 'rubeus', '=0.0.4'
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', '..', 'lib')
+#require 'rubygems'
+#gem 'rubeus'
+
 require 'rubeus'
 Rubeus::Swing.irb
+require File.join(File.dirname(__FILE__), 'nyanco_disp_label')
 
-JFrame.new('にゃんこびゅーあー Rubeus') do |frame|
+JFrame.new('にゃんこびゅーあー Rubeus ' + Rubeus::VERSION) do |frame|
   frame.set_size 400, 300
   frame.default_close_operation = JFrame::EXIT_ON_CLOSE
   frame.icon_image = ImageIcon.new(File.join(File.dirname(__FILE__), 'nekobean_s.png')).image
 
   JPanel.new do |base_pane|
-    base_pane.layout = java.awt.BorderLayout.new
+    base_pane.layout = BorderLayout.new
     JScrollPane.new do |sp|
-      layout = java.awt.FlowLayout.new
-      layout.alignment = java.awt.FlowLayout::LEFT
+      layout = FlowLayout.new
+      layout.alignment = FlowLayout::LEFT
       sp.viewport.layout = layout
 
-      require File.join(File.dirname(__FILE__), 'nyanco_disp_label')
       lb = NyancoDispLabel.new
 
       #ドラッグ&ドロップを有効に
       [lb, sp].each do |dt|
-        java.awt.dnd.DropTarget.new dt, lb
+        DropTarget.new dt, lb
         dt.tool_tip_text = 'にゃんこの画像をここにポイッ♪'
       end
     end

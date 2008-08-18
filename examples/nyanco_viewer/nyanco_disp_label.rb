b1905c5d45c77a8c46dc7e8e1d77c651f4e0bf4b
Takahiro.Shigemoto <Takahiro.Shigemoto@aae16913-fd4f-0410-af92-35f713787d03>
2008-08-18 16:35:37 +0000

    add text/uri-list support.
    git-svn-id: https://rubeus.googlecode.com/svn/trunk@138 aae16913-fd4f-0410-af92-35f713787d03

--- examples/nyanco_viewer/nyanco_disp_label.rb
+++ examples/nyanco_viewer/nyanco_disp_label.rb
@@ -24,15 +24,24 @@ class NyancoDispLabel < javax.swing.JLabel
   include DD
   def drop(e)
     e.accept_drop java.awt.dnd.DnDConstants::ACTION_COPY_OR_MOVE
+
+    # Supported Flavors
     java_file_list_flavor = java.awt.datatransfer.DataFlavor.javaFileListFlavor
     string_flavor = java.awt.datatransfer.DataFlavor.stringFlavor
+    uri_flavor = java.awt.datatransfer.DataFlavor.new("text/uri-list;class=java.lang.String")
+
     if e.transferable.isDataFlavorSupported(java_file_list_flavor)
       image_path = e.transferable.get_transfer_data(java_file_list_flavor)[0].absolute_path
     elsif e.transferable.isDataFlavorSupported(string_flavor)
-      image_path = java.net.URL.new(e.transferable.get_transfer_data(string_flavor))
+      image_path = java.net.URL.new(e.transferable.get_transfer_data(string_flavor).split("\r\n")[0])
+    elsif e.transferable.isDataFlavorSupported(uri_flavor)
+      image_path = java.net.URL.new(e.transferable.get_transfer_data(uri_flavor).split("\r\n")[0])
     else
       # Unsupported data
     end 
-    self.icon = javax.swing.ImageIcon.new(image_path)
+
+    if image_path
+      self.icon = javax.swing.ImageIcon.new(image_path)
+    end
   end
 end

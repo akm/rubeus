c8b44500c0af95c7f2f36e38814239f4a0238c91
Takahiro.Shigemoto <Takahiro.Shigemoto@aae16913-fd4f-0410-af92-35f713787d03>
2008-08-21 12:33:26 +0000

    Add Timer extension
    git-svn-id: https://rubeus.googlecode.com/svn/trunk@180 aae16913-fd4f-0410-af92-35f713787d03

---
+++ lib/rubeus/extensions/javax/swing/timer.rb
@@ -0,0 +1,26 @@
+module Rubeus::Extensions::Javax::Swing
+  module Timer
+    def self.included(base)
+      base.extend ClassMethods
+      base.instance_eval do
+        alias :new_without_rubeus :new
+        alias :new :new_with_rubeus
+      end
+    end
+ 
+    module ClassMethods
+      def new_with_rubeus(interval, &block)
+        # Create ActionListener implement class
+        mod = Module.new do
+          define_method("actionPerformed", &block)
+        end
+ 
+        obj = Object.new
+        obj.extend(mod)
+ 
+        # Use original constructor
+        new_without_rubeus(interval, obj)
+      end
+    end
+  end
+end

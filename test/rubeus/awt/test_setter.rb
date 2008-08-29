5e90fbb17d47cc0dc61caa4f617388bf3891f62f
Takahiro.Shigemoto <Takahiro.Shigemoto@aae16913-fd4f-0410-af92-35f713787d03>
2008-08-29 00:15:05 +0000

    Add setter.rb test
    git-svn-id: https://rubeus.googlecode.com/svn/trunk@191 aae16913-fd4f-0410-af92-35f713787d03

---
+++ test/rubeus/awt/test_setter.rb
@@ -0,0 +1,42 @@
+require 'test/unit'
+require 'rubygems'
+require 'rubeus'
+
+# Test for setter.rb
+class TestSetter < Test::Unit::TestCase
+  include Rubeus::Swing
+
+  # setup method
+  def setup
+  end
+
+  # test set_preferred_size
+  def test_set_preferred_size
+    JFrame.new do |f|
+      txt = JTextField.new
+      txt.set_preferred_size "100 x 150"
+
+      f.pack
+
+      assert_equal(100, txt.get_preferred_size.width)
+      assert_equal(150, txt.get_preferred_size.height)
+
+      f.dispose
+    end
+  end
+
+  # test preferred_size=
+  def test_preferred_size=
+    JFrame.new do |f|
+      txt = JTextField.new
+      txt.preferred_size = "150 x 100"
+
+      f.pack
+
+      assert_equal(150, txt.preferred_size.width)
+      assert_equal(100, txt.preferred_size.height)
+
+      f.dispose
+    end
+  end
+end

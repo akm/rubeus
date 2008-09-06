a69668a01384d1885ea48ad222c13c2620bd4a5a
Takahiro.Shigemoto <Takahiro.Shigemoto@aae16913-fd4f-0410-af92-35f713787d03>
2008-09-06 15:36:14 +0000

    Add driver_manager.rb test
    git-svn-id: https://rubeus.googlecode.com/svn/trunk@208 aae16913-fd4f-0410-af92-35f713787d03

---
+++ test/rubeus/extensions/java/sql/test_driver_manager.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+require 'rubygems'
+require 'rubeus'
+
+# Test for driver_manager.rb
+class TestDriverManager < Test::Unit::TestCase
+  # setup method
+  def setup
+  end
+
+  def test_normal
+    assert_nothing_raised do
+      @con = Rubeus::Jdbc::DriverManager.connect("jdbc:derby:test;create = true", "", "")
+    end
+
+    if @con
+      assert_equal(false, @con.closed?)
+      @con.close
+    end
+  end
+
+  def test_without_auto_setup_manager
+    assert_nothing_raised do
+      org.apache.derby.jdbc.EmbeddedDriver
+      @con = Rubeus::Jdbc::DriverManager.connect("jdbc:derby:test;create = true", "", "", :auto_setup_manager => false)
+      @con.close
+    end
+  end
+
+  def test_with_block
+    con = Rubeus::Jdbc::DriverManager.connect("jdbc:derby:test;create = true") do |con|
+      assert_equal(false, con.closed?)
+    end
+    assert_nil(con)
+  end
+end
+

19eee18a69f39a9f2b3b4d0bc7c7efb5612f48e0
akm2000 <akm2000@aae16913-fd4f-0410-af92-35f713787d03>
2008-12-11 17:12:31 +0000

    create_table_after_dropをちょっと修正しました。
    git-svn-id: https://rubeus.googlecode.com/svn/trunk@233 aae16913-fd4f-0410-af92-35f713787d03

--- test/rubeus/extensions/java/sql/test_sql_helper.rb
+++ test/rubeus/extensions/java/sql/test_sql_helper.rb
@@ -29,7 +29,7 @@ module TestSqlHelper
   
   def create_table_after_drop(create_ddl, statement = nil)
     create_ddls = create_ddl.split(/\;/mi).map{|ddl| ddl.strip }.select{|ddl| !(ddl.nil? || ddl.empty?) }
-    table_names = create_ddls.map{|ddl| ddl.scan(/^\s*CREATE TABLE\s+?(.+?)[\s$]/mi)}
+    table_names = create_ddls.map{|ddl| ddl.scan(/^\s*CREATE TABLE\s+?(.+?)[\s\($]/mi)}
     close_if_no_statement(statement) do |stmt|
       table_names.reverse.each do |table_name|
         drop_table_if_exist(table_name, stmt)

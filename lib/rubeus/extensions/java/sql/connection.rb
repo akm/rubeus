798736fd8d8c7e4c85ee489a8ff4fbc18d7a3ed4
akm2000 <akm2000@aae16913-fd4f-0410-af92-35f713787d03>
2008-12-11 17:11:16 +0000

    java.sql.Connectionにtablesメソッドを作成しました。
    git-svn-id: https://rubeus.googlecode.com/svn/trunk@228 aae16913-fd4f-0410-af92-35f713787d03

--- lib/rubeus/extensions/java/sql/connection.rb
+++ lib/rubeus/extensions/java/sql/connection.rb
@@ -4,13 +4,17 @@ Rubeus::Jdbc.depend_on("DatabaseMetaData")
 module Rubeus::Extensions::Java::Sql
   module Connection
     include Rubeus::Jdbc::CloseableResource
-
+    
     def statement(*args, &block)
       with_close(create_statement(*args), &block)
     end
 
     def query(sql, &block)
       statement{|st| st.query(sql, &block)}
+    end
+    
+    def tables(catalog, schema_pattern, table_name_pattern, options = nil)
+      getMetaData.table_objects(catalog, schema_pattern, table_name_pattern, options = nil)
     end
   end
 end

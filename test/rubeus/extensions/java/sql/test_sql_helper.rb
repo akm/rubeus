2fa630c6ca04c60042e9d19cc1537f3032dfdb98
akima <akm2000@gmail.com>
2013-09-07 15:29:52 +0900

    raise error unless fail to drop table

--- test/rubeus/extensions/java/sql/test_sql_helper.rb
+++ test/rubeus/extensions/java/sql/test_sql_helper.rb
@@ -21,17 +21,22 @@ module TestSqlHelper
 
   def drop_table_if_exist(table_name, statement = nil)
     close_if_no_statement(statement) do |stmt|
+      sql = "DROP TABLE #{table_name}"
       begin
-        stmt.execute_update("DROP TABLE #{table_name}")
-      rescue
-        # the table doesn't exist but do nothing
+        stmt.execute_update(sql)
+      rescue => e
+        if e.message =~ /it does not exist/
+          # the table doesn't exist but do nothing
+        else
+          raise e
+        end
       end
     end
   end
 
   def create_table_after_drop(create_ddl, statement = nil)
     create_ddls = create_ddl.split(/\;/mi).map{|ddl| ddl.strip }.select{|ddl| !(ddl.nil? || ddl.empty?) }
-    table_names = create_ddls.map{|ddl| ddl.scan(/^\s*CREATE TABLE\s+?(.+?)[\s\($]/mi)}
+    table_names = create_ddls.map{|ddl| ddl.scan(/^\s*CREATE TABLE\s+?(.+?)[\s\($]/mi)}.flatten
     close_if_no_statement(statement) do |stmt|
       table_names.reverse.each do |table_name|
         drop_table_if_exist(table_name, stmt)
